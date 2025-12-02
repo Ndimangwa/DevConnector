ba3ebca76f6ec3e0a871c7b7f4845b530d9c0ad8
ndimangwa <ndimangwa@gmail.com>
2025-12-02 12:38:39 +0300

    Connected with MongoDB

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+//connectDB
+connectDB();
+
+//Testing route
+app.get('/', (req, res) => {
+    res.send('App is Running!!!!!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started at port : ${PORT}`);
+});
