e02acf69f806272ee5c30b339da086920181bcc2
ndimangwa <ndimangwa@gmail.com>
2025-12-25 12:20:25 +0300

    Working with posts

---
+++ postman/posts/get_post_with_id.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ $# -lt 2 ]]; then 
+	echo "Usage : $0 <token_value> <post_id>"
+	exit 1
+fi
+curl -X GET "127.0.0.1:5000/api/posts/$2" -H "Content-Type: application/json" -H "x-auth-token: $1"
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,152 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const auth = require('../../middleware/auth');
 
+//@route        POST /api/posts
+//@desc         Create Posts
+//@access       Private
+router.post('/', [
+    auth,   [
+        check('text', 'Text is required')
+            .not()
+            .isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (! errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        //fetching User model
+        const user = await User.findById(req.user.id).select('-password');
+        //fetching Post model
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        //saving
+        const post = await newPost.save();
+        //now returning post
+        return res.status(201).json(post);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    }
+});
 //@route        GET /api/posts
-//@desc         Test Route
-//@access       Public
-router.get('/', (req, res) => res.send('Posts route'));
+//@desc         Get All Posts
+//@access       Private -- Posts need to be accessed by logged in user
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        return res.status(200).json(posts);
+    } catch (err) {
+       console.error(err.message);
+       return res.status(500).send('Server Error'); 
+    }
+});
+//@route        GET /api/posts/:id
+//@desc         Get specific post
+//@access       Private -- Posts need to be accessed by logged in user
+router.get('/:id', auth, async (req, res) => {
+    const postNotFound = 'Post Not Found';
+    try {
+        const post = await Post.findById(req.params.id);
+        if (! post) {
+            return res.status(404).json({ msg: postNotFound });
+        }
+        return res.status(200).json(post);
+    } catch (err) {
+        if (err.kind === 'ObjectId')    {
+            return res.status(404).json({ msg: postNotFound });
+        }
+       return res.status(500).send('Server Error'); 
+    }
+});
+//@route        DELETE /api/posts/:id
+//@desc         Delete a specific post
+//@access       Private -- Posts need to be accessed by logged in user
+router.delete('/:id', auth, async (req, res) => {
+    const postNotFound = 'Post Not Found';
+    try {
+        const post = await Post.findById(req.params.id);
+        if (! post) {
+            return res.status(404).json({ msg: postNotFound });
+        }
+        //Make sure that the post we delete belong to this user
+        if (post.user.toString() !== req.user.id)   {
+            return res.status(401).json({ msg: 'User not Authorized' });
+        }
+        //Delete now this post
+        await post.deleteOne(); //Need to work remove or delete
+        return res.status(200).json({ msg: 'Post removed successful' });
+    } catch (err) {
+        if (err.kind === 'ObjectId')    {
+            return res.status(404).json({ msg: postNotFound });
+        }
+       return res.status(500).send('Server Error'); 
+    }
+});
+//Working with Likes
+//@route        PUT /api/posts/like/:id
+//@desc         Add like to a post with id, you can only deal with your like
+//@access       Private -- Posts need to be accessed by logged in user
+router.put('/like/:id', auth, async (req, res) => {
+    const postNotFound = 'Post Not Found';
+    try {
+        const post = await Post.findById(req.params.id);
+        if (! post) {
+            return res.status(404).json({ msg: postNotFound });
+        }
+        //Check if the post has already been liked by the user
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0)    {
+            return res.status(400).json({ msg: "Post Already Liked" });
+        }
+        //Now proceed with actual business
+        post.likes.unshift({ user: req.user.id });
+        await post.save();
+        return res.status(200).json(post.likes);
+    } catch (err) {
+        if (err.kind === 'ObjectId')    {
+            return res.status(404).json({ msg: postNotFound });
+        }
+       return res.status(500).send('Server Error'); 
+    }
+});
+//Working with Likes
+//@route        PUT /api/posts/unlike/:id
+//@desc         Remove the previous like, you can deal with only your like
+//@access       Private -- Posts need to be accessed by logged in user
+router.put('/unlike/:id', auth, async (req, res) => {
+    const postNotFound = 'Post Not Found';
+    try {
+        const post = await Post.findById(req.params.id);
+        if (! post) {
+            return res.status(404).json({ msg: postNotFound });
+        }
+        //Check if the post has already been liked by the user
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0)    {
+            return res.status(400).json({ msg: "The post has not been yet liked" });
+        }
+        //Now proceed with actual business
+        //get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        post.likes.splice(removeIndex, 1);
+        //Performing saving
+        await post.save();
+        return res.status(200).json(post.likes);
+    } catch (err) {
+        if (err.kind === 'ObjectId')    {
+            return res.status(404).json({ msg: postNotFound });
+        }
+       return res.status(500).send('Server Error'); 
+    }
+});
+
 
 module.exports = router;
